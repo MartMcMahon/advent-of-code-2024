4c244a6cc78bc61679a1a2cce14a5c8833cd0de5
Mart <themcman@gmail.com>
2024-12-02 23:59:56 -0600

    day 3 - done

--- 03/day03.py
+++ 03/day03.py
@@ -3,16 +3,36 @@ import re
 with open("input.txt", "r") as f:
     input_text = f.read()
 
-pattern = r"mul\(\d+,\d+\)"
-matches = re.findall(pattern, input_text)
 
-sum = 0
-for m in matches:
-    s = m.split(",")
-    factor1 = int(s[0][4:])
-    factor2 = int(s[1][:-1])
+def get_muls(do_text):
+    print(do_text)
+    pattern = r"mul\(\d+,\d+\)"
+    matches = re.findall(pattern, do_text)
 
-    sum += factor1 * factor2
+    sum = 0
+    for m in matches:
+        s = m.split(",")
+        factor1 = int(s[0][4:])
+        factor2 = int(s[1][:-1])
+
+        print(m, " = " + str(factor1 * factor2))
+        sum += factor1 * factor2
+        print("sum is " + str(sum))
+
+    return sum
 
 
-print(sum)
+def dos_and_donts(input_text):
+    s1 = input_text.split("do()")
+    # s1 = ["do.......dont.......", ..]
+    strings = map(lambda x: x.split("don't()")[0], s1)
+    # strings = ["do...", ]
+
+    grand_sum = 0
+    for s in strings:
+        grand_sum += get_muls(s)
+
+    return grand_sum
+
+
+print(dos_and_donts(input_text))
